754903f12f41c3188454c726d42d73bb63742c51
Rafael Ornelas <rornelas@macrafael.local>
2015-09-22 15:02:32 -0300

    Initial Commit

--- TaskList/SecondViewController.swift
+++ TaskList/SecondViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        toDoList.append(item.text!)
+        item.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(texField: UITextField!) -> Bool {
+        item.resignFirstResponder()
+        return true
     }
 
 
